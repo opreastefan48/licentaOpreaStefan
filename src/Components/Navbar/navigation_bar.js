2961355bbcc09f97caf20e99430cc6a024a232d8
opreastefan48 <opreastefan48@yahoo.com>
2023-06-24 18:09:04 +0300

    worksx2

--- src/Components/Navbar/navigation_bar.js
+++ src/Components/Navbar/navigation_bar.js
@@ -1,6 +1,6 @@
 import { useState, useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import "./navbar.css";
 
@@ -25,21 +25,23 @@ function Nav_bar() {
   return (
     <header>
       <h3>Afine Oprea</h3>
-      <nav ref={navRef}>
-        <Link to="/home"> Home </Link>
-        <Link to="/probleme"> Probleme </Link>
-        <Link to="/cules"> Adauga Cantitate Culeasa </Link>
-        <Link to="/total"> Total cules </Link>
-        <Link to="/fertilizare"> Tabele Fertilizare </Link>
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <FaTimes />
-        </button>
-        {isLoggedIn ? (
-          <Button onClick={logout} variant="danger">LOGOUT</Button>
-        ) : (
-          <Button onClick={login} variant="primary">LOGIN</Button>
-        )}
-      </nav>
+      <Router> {/* Wrap the component with Router */}
+        <nav ref={navRef}>
+          <Link to="/home"> Home </Link>
+          <Link to="/probleme"> Probleme </Link>
+          <Link to="/cules"> Adauga Cantitate Culeasa </Link>
+          <Link to="/total"> Total cules </Link>
+          <Link to="/fertilizare"> Tabele Fertilizare </Link>
+          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+            <FaTimes />
+          </button>
+          {isLoggedIn ? (
+            <Button onClick={logout} variant="danger">LOGOUT</Button>
+          ) : (
+            <Button onClick={login} variant="primary">LOGIN</Button>
+          )}
+        </nav>
+      </Router>
       <button className="nav-btn" onClick={showNavbar}>
         <FaBars />
       </button>
